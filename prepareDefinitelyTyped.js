bfe85454cb85ad24acb614a18738f229dada41b6
Rifa'i M. Hanif <hanreev@gmail.com>
2019-05-17 02:13:15 +0700

    lint using dtslint

--- prepareDefinitelyTyped.js
+++ prepareDefinitelyTyped.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra');
 const path = require('path');
 const glob = require('glob');
+const childProcess = require('child_process');
 
 const configs = {
   tsconfig: {
@@ -46,18 +47,49 @@ const header = `// Type definitions for ol 5.3
 `;
 
 const srcPath = path.resolve(__dirname, '@types', 'ol');
-const destPath = path.resolve(__dirname, 'DefinitelyTyped', 'types', 'ol');
+const dtPath = path.resolve(__dirname, 'DefinitelyTyped');
+const destPath = path.join(dtPath, 'types', 'ol');
+
+console.log('# Preparing definitions for DefinitelyTyped');
 
 // Clean
-fs.removeSync(destPath);
+console.log('# Cleaning output directory');
+fs.removeSync(dtPath);
+fs.mkdirpSync(destPath);
+
+// Copy .d.ts files
+console.log('# Copying definition files');
 fs.copySync(srcPath, destPath);
 
+console.log('# Generating tsconfig.json and tslint.json');
+// Append .d.ts path to tsconfig files
 glob.sync(path.join(destPath, '**', '*.d.ts')).forEach(dtsPath => {
   configs.tsconfig.files.push(path.relative(destPath, dtsPath));
 });
 
+// Write tsconfig.json and tslint.json
 for (const key in configs)
   fs.writeFileSync(path.join(destPath, key + '.json'), JSON.stringify(configs[key], null, 4));
 
-const indexPath = path.join(destPath, 'index.d.ts')
+// Prepend DefinitelyTyped header to index.d.ts
+console.log('# Adding DefinitelyTyped header');
+const indexPath = path.join(destPath, 'index.d.ts');
 fs.writeFileSync(indexPath, header + fs.readFileSync(indexPath).toString());
+
+// Lint using dtslint
+console.log('# Linting definitions using dtslint');
+const dtPackageJson = {
+  name: 'DefinitelyTyped',
+  version: '1.0.0',
+  scripts: {
+    lint: 'dtslint types'
+  }
+};
+process.chdir(dtPath);
+fs.writeFileSync('package.json', JSON.stringify(dtPackageJson, null, 4));
+childProcess.execSync('npm i -D dtslint', { stdio: 'inherit' });
+childProcess.execSync('npm run lint ol', { stdio: 'inherit' });
+fs.readdirSync(dtPath).forEach(filename => filename != 'types' && fs.removeSync(filename));
+process.chdir(__dirname);
+
+console.log('# DONE');
--- prepareNpm.js
+++ prepareNpm.js
@@ -5,23 +5,32 @@ const childProcess = require('child_process');
 const srcPath = path.resolve(__dirname, '@types', 'ol');
 const destPath = path.resolve(__dirname, 'ol');
 
+console.log('# Preparing definitions before publishing to npmjs.org');
+
 // Clean
+console.log('# Cleaning output directory');
 fs.removeSync(destPath);
 
 // Build
+console.log('# Rebuilding definitions');
 childProcess.execSync('npm run build', { stdio: 'inherit' });
+console.log('# Formatting definitions');
 childProcess.execSync('npm run format', { stdio: 'inherit' });
 
 // Lint
+console.log('# Linting definitions');
 childProcess.execSync('npm run lint', { stdio: 'inherit' });
 
 // Test
-childProcess.execSync('npm run test', { stdio: 'inherit' });
+console.log('# Testing definitions');
+childProcess.execSync('npm run lint-test && npm run test', { stdio: 'inherit' });
 
 // Copy
+console.log('# Emitting definitions to output directory');
 fs.copySync(srcPath, destPath);
 
 // Add package.json
+console.log('# Copying package.json');
 const packageJsonPath = path.resolve(__dirname, 'package.json');
 const packageJsonDestPath = path.join(destPath, 'package.json');
 
@@ -31,3 +40,5 @@ delete src.devDependencies;
 delete src.files;
 
 fs.writeFileSync(packageJsonDestPath, JSON.stringify(src, null, 4));
+
+console.log('# DONE');
