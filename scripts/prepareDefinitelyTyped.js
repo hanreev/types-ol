cfe679e2449cc0f8a052266012aa255ef242b4aa
Rifa'i M. Hanif <hanreev@gmail.com>
2020-01-07 03:08:42 +0700

    Update to OpenLayers v6.1.1

--- scripts/prepareDefinitelyTyped.js
+++ scripts/prepareDefinitelyTyped.js
@@ -3,6 +3,9 @@ const path = require('path');
 const glob = require('glob');
 const childProcess = require('child_process');
 
+const { getDefinitelyTyped, consoleLogger } = require('types-publisher');
+const { allReferencedFiles } = require('types-publisher/bin/lib/module-info');
+
 const BASE_DIR = process.cwd();
 
 const jsdocConfig = require(path.resolve(BASE_DIR, 'jsdoc', 'conf.json'));
@@ -24,7 +27,7 @@ const configs = {
       typeRoots: ['../'],
       types: [],
     },
-    files: [],
+    files: ['index.d.ts'],
   },
   tslint: require(path.join(BASE_DIR, 'tslint.json')),
 };
@@ -34,6 +37,7 @@ const header = `// Type definitions for ol ${olVersion}
 // Definitions by: Rifa'i M. Hanif <https://github.com/hanreev>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.1
+// Minimum TypeScript Version: 3.1
 
 // These definitions was generated using jsdoc. See https://github.com/hanreev/types-ol
 
@@ -43,55 +47,107 @@ const srcPath = path.resolve(BASE_DIR, '@types', 'ol');
 const dtPath = path.resolve(BASE_DIR, 'DefinitelyTyped');
 const destPath = path.join(dtPath, 'types', 'ol');
 
-console.log('# Preparing definitions for DefinitelyTyped');
+/**
+ * Prepare DefinitelyTyped
+ */
+function prepareDt() {
+  console.log('# Preparing definitions for DefinitelyTyped');
 
-// Clean
-console.log('# Cleaning output directory');
-fs.removeSync(dtPath);
-fs.mkdirpSync(destPath);
+  // Clean
+  console.log('# Cleaning output directory');
+  fs.removeSync(dtPath);
+  fs.mkdirpSync(destPath);
 
-// Copy .d.ts files
-console.log('# Copying definition files');
-fs.copySync(srcPath, destPath);
+  const dtPackageJson = {
+    name: 'DefinitelyTyped',
+    version: '1.0.0',
+    license: 'UNLICENSED',
+    scripts: {
+      lint: 'dtslint types',
+      test: 'node node_modules/types-publisher/bin/tester/test.js --run-from-definitely-typed',
+    },
+    dependencies: {
+      dtslint: 'latest',
+      'types-publisher': 'github:Microsoft/types-publisher#production',
+    },
+  };
+  const dtNotNeededPackagesJson = { packages: [] };
+  const gitignorePaths = ['/node_modules', 'yarn.lock', 'yarn-error.log'];
+  process.chdir(dtPath);
+  fs.writeFileSync('package.json', JSON.stringify(dtPackageJson, null, 4), { encoding: 'utf-8' });
+  fs.writeFileSync('notNeededPackages.json', JSON.stringify(dtNotNeededPackagesJson, null, 4), { encoding: 'utf-8' });
+  fs.writeFileSync('.gitignore', gitignorePaths.join('\n'), { encoding: 'utf-8' });
+  process.chdir(dtPath);
+  childProcess.execSync('git init', { stdio: 'inherit' });
+  childProcess.execSync('git add --all', { stdio: 'inherit' });
+  childProcess.execSync('git commit -m initial', { stdio: 'inherit' });
+  process.chdir(BASE_DIR);
+}
 
-console.log('# Generating tsconfig.json and tslint.json');
-// Append .d.ts path to tsconfig files
-glob.sync(path.join(destPath, '**', '*.d.ts')).forEach(dtsPath => {
-  configs.tsconfig.files.push(path.relative(destPath, dtsPath).replace(/\\/g, '/'));
+/**
+ * Copy ol definitions
+ */
+function copyDefinitions() {
+  // Copy .d.ts files
+  console.log('# Copying definition files');
+  fs.copySync(srcPath, destPath);
+
+  console.log('# Generating tsconfig.json and tslint.json');
+
+  // Copy test files
+  console.log('# Copying test files');
+  fs.copyFileSync(path.resolve('test', 'ol-tests.ts'), path.join(destPath, 'ol-tests.ts'));
+  configs.tsconfig.files.push('ol-tests.ts');
+
+  // Write tsconfig.json and tslint.json
+  for (const key in configs)
+    fs.writeFileSync(path.join(destPath, key + '.json'), JSON.stringify(configs[key], null, 4), { encoding: 'utf-8' });
+
+  // Prepend DefinitelyTyped header to index.d.ts
+  console.log('# Adding DefinitelyTyped header');
+  const indexPath = path.join(destPath, 'index.d.ts');
+  fs.writeFileSync(indexPath, header + fs.readFileSync(indexPath, 'utf-8'));
+}
+
+/**
+ * Write OTHER_FILES.txt
+ */
+async function writeOtherFiles() {
+  console.log('# Listing and writing other files');
+
+  const options = { definitelyTypedPath: dtPath, progress: true, parseInParallel: true };
+  const dt = await getDefinitelyTyped(options, consoleLogger);
+  const dtFs = dt.subDir('types').subDir('ol');
+  const entryFiles = configs.tsconfig.files;
+  const { types, tests } = allReferencedFiles(entryFiles, dtFs, 'ol', path.resolve('./DefinitelyTyped/types/ol'));
+  const usedFiles = new Set([...types.keys(), ...tests.keys(), 'tsconfig.json', 'tslint.json']);
+  const otherFiles = new Set();
+  glob.sync(path.join(destPath, '**', '*.d.ts')).forEach(dtsPath => {
+    const dtsRelpath = path.relative(destPath, dtsPath).replace(/\\/g, '/');
+    if (!usedFiles.has(dtsRelpath)) otherFiles.add(dtsRelpath);
+  });
+  fs.writeFileSync(path.join(destPath, 'OTHER_FILES.txt'), Array.from(otherFiles).join('\n'), { encoding: 'utf-8' });
+}
+
+/**
+ * Lint and test
+ */
+async function lintAndTest() {
+  console.log('# Linting and testing definitions');
+  process.chdir(dtPath);
+  await writeOtherFiles();
+  childProcess.execSync('git add --all', { stdio: 'inherit' });
+  childProcess.execSync('git commit -m types-ol', { stdio: 'inherit' });
+  childProcess.execSync('yarn install', { stdio: 'inherit' });
+  childProcess.execSync('yarn lint ol', { stdio: 'inherit' });
+  childProcess.execSync('yarn test', { stdio: 'inherit' });
+
+  console.log('# Cleanup DefinitelyTyped directory');
+  fs.readdirSync(dtPath).forEach(filename => filename != 'types' && fs.removeSync(filename));
+  process.chdir(BASE_DIR);
+}
+prepareDt();
+copyDefinitions();
+lintAndTest().then(() => {
+  console.log('# DONE');
 });
-
-// Copy test files
-console.log('# Copying test files');
-fs.copyFileSync(path.resolve('test', 'ol-tests.ts'), path.join(destPath, 'ol-tests.ts'));
-configs.tsconfig.files.push('ol-tests.ts');
-
-// Write tsconfig.json and tslint.json
-for (const key in configs) fs.writeFileSync(path.join(destPath, key + '.json'), JSON.stringify(configs[key], null, 4));
-
-// Prepend DefinitelyTyped header to index.d.ts
-console.log('# Adding DefinitelyTyped header');
-const indexPath = path.join(destPath, 'index.d.ts');
-fs.writeFileSync(indexPath, header + fs.readFileSync(indexPath).toString());
-
-// Lint using dtslint
-console.log('# Linting definitions using dtslint');
-const dtPackageJson = {
-  name: 'DefinitelyTyped',
-  version: '1.0.0',
-  scripts: {
-    lint: 'dtslint types',
-  },
-  dependencies: {
-    dtslint: 'latest',
-  },
-};
-process.chdir(dtPath);
-fs.writeFileSync('package.json', JSON.stringify(dtPackageJson, null, 4));
-childProcess.execSync('yarn install', { stdio: 'inherit' });
-childProcess.execSync('yarn lint ol', { stdio: 'inherit' });
-
-console.log('# Cleanup DefinitelyTyped directory');
-fs.readdirSync(dtPath).forEach(filename => filename != 'types' && fs.removeSync(filename));
-process.chdir(BASE_DIR);
-
-console.log('# DONE');
